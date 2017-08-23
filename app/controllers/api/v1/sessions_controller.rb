6f61e27954f0b5f779e050137cd4eb309ee5e9c0
cheungth25 <cheungth25@gmail.com>
2017-08-23 09:47:12 -0400

    now sending userid on login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::SessionsController < ApplicationController
     if @user && @user.authenticate(params[:password])
       payload = {user_id: @user.id}
       token = issue_token(payload)
-      render json: {not_jwt: token}
+      render json: {not_jwt: token, user_id: @user.id}
     else
       render json: {message: "You fucked up boi"}
     end
